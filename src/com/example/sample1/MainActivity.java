f1f3841fce451eaa90f37c86ef5cb3e37f5794c1
Susmithapochiraju <pochirajususmitha@gmail.com>
2015-07-27 15:05:51 +0530

    Initial commited file

---
+++ src/com/example/sample1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sample1;
+
+import org.apache.cordova.DroidGap;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	   super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
